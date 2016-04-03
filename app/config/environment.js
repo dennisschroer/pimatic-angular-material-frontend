1c7512d518deae1a78632b77c9eda0d4808bce32
Dennis <dennisschroer@hotmail.com>
2016-04-03 21:52:03 +0200

    Update environment config

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,10 +1,5 @@
 angular.module('pimaticApp.configuration').provider('config', function () {
-    this.development = {
-        title: 'Pimatic frontend - DEV',
-        pimaticHost: '',
-        apiName: 'fixtureApi',
-        debug: true
-    };
+    this.environment = 'development';
 
     this.production = {
         title: '',
@@ -13,7 +8,23 @@ angular.module('pimaticApp.configuration').provider('config', function () {
         debug: false
     };
 
+    this.development = {
+        title: 'Pimatic frontend - DEV',
+        pimaticHost: '',
+        apiName: 'fixtureApi',
+        debug: true
+    };
+
+    this.testing = this.development;
+
     this.$get = function () {
-        return this.development;
+        switch (this.environment){
+            case 'testing':
+                return this.testing;
+            case 'development':
+                return this.development;
+            case 'production':
+                return this.production;
+        }
     }
 });
