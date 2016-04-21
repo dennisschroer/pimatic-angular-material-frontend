27134cc3dd218ed5eb3e28d201ae3de5dfdf2cb0
Dennis <dennisschroer@hotmail.com>
2016-04-21 23:45:15 +0200

    Cleanup

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -28,24 +28,4 @@ angular.module('pimaticApp').controller('HomeController', ["$scope", "$filter", 
     $scope.getDevice = function (deviceId) {
         return store.get('devices', deviceId)
     };
-
-    /*$scope.selectPage = function(){
-     console.log('selectPage', $routeParams.pageId);
-     if(!angular.isUndefined($routeParams.pageId)){
-     angular.forEach($scope.pages, function(value, key){
-     if(value.id == $routeParams.pageId){
-     $scope.selectedTab = key;
-     }
-     });
-     }
-     };
-
-     $scope.tabSelected = function(page){
-     console.log('tabSelected', page);
-     $location.path('home/' + page.id);
-     }
-
-     $scope.$watch($scope.pages, function(newVal, oldVal){
-     $scope.selectPage();
-     }, true);*/
 }]);
